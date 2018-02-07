128409fcbfd3d2a1038e20e8166568bb850a09ef
cesslab <anwarruff@gmail.com>
2018-02-07 08:38:08 -0500

    added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='vprinter',
+      version='0.1',
+      description='CESS Voucher Printer',
+      url='http://github.com/cesslab/Lab-VoucherPrinter',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['vprinter'],
+      zip_safe=False)
