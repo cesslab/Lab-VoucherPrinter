e0cf111c9117d36bee9fee9a9f94e699b79d20c7
aaruff <anwarruff@gmail.com>
2018-02-07 13:41:29 -0500

    added total row var

--- vprinter/ui.py
+++ vprinter/ui.py
@@ -13,22 +13,6 @@ class PrimaryFrame(tk.Frame):
 
         self.voucher_printer = voucher_printer
 
-        self.label = ttk.Label(self, text="File Path:", font=('Arial', 12))
-        self.label.pack(pady=10, padx=10)
-
-
-class ResultsFrame(tk.Frame):
-    def __init__(self, root_frame, voucher_printer):
-        tk.Frame.__init__(self, root_frame)
-
-        self.voucher_printer = voucher_printer
-
-        label = ttk.Label(self, text="Results", font=('Arial', 12))
-        label.pack(pady=10, padx=10)
-
-        button1 = ttk.Button(self, text="Back", command=lambda: root_frame.show_frame(PrimaryFrame))
-        button1.pack()
-
 
 class VoucherPrinterGUI(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -59,28 +43,51 @@ class VoucherPrinterGUI(tk.Tk):
 
         self.voucher = Voucher()
 
+        self.total_rows = 1
+
         self.show_frame(ui.PrimaryFrame)
 
     def show_frame(self, frame):
         current_frame = self.frames[frame]
         current_frame.tkraise()
 
+    def calibrate(self):
+        pass
+
     def build_table(self, subjects, payoffs):
         rows = len(subjects)
         for row in range(1, rows):  # Rows
             a = ttk.Entry(self)
             a.insert(tk.END, subjects[row])
-            a.grid(row=row, column=0)
+            a.grid(row=self.total_rows, column=0)
+
             b = ttk.Entry(self)
             b.insert(tk.END, payoffs[row])
-            b.grid(row=row, column=1)
+            b.grid(row=self.total_rows, column=1)
+
+            self.total_rows += 1
+
+        button1 = ttk.Button(self, text="Calibrate", command=lambda: self.calibrate())
+        button1.grid(row=self.total_rows, column=0)
 
     def open_payoff_file(self):
         file_name = filedialog.askopenfilename(parent=self, filetypes=[("File", "*.pay")])
-        self.frames[ui.PrimaryFrame].label.config(text='File: ' + file_name)
         if self.voucher.open_file(file_name):
             self.build_table(self.voucher.subjects, self.voucher.payoffs)
         else:
             messagebox.showerror("Error", "Unable to open file. Error: {}.".format(self.voucher.error))
+
+
+class ResultsFrame(tk.Frame):
+    def __init__(self, root_frame, voucher_printer):
+        tk.Frame.__init__(self, root_frame)
+
+        self.voucher_printer = voucher_printer
+
+        label = ttk.Label(self, text="Results", font=('Arial', 12))
+        label.pack(pady=10, padx=10)
+
+        button1 = ttk.Button(self, text="Back", command=lambda: root_frame.show_frame(PrimaryFrame))
+        button1.pack()
 
 
