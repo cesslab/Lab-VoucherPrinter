bc0c1c0d88c1007fa639319c5ab13858e503372c
cesslab <anwarruff@gmail.com>
2018-02-15 07:06:13 -0500

    added toolbar open button

--- vprinter/ui.py
+++ vprinter/ui.py
@@ -1,65 +1,77 @@
 import os
 import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog
-from tkinter import messagebox
-from tkinter import Button
+from tkinter import ttk, filedialog, messagebox
 from PIL import Image, ImageTk
 
-from vprinter import ui
 from vprinter.voucher import Voucher
 from vprinter.configuration import ConfigurationSettings
 
 
-class PrimaryFrame(tk.Frame):
-    def __init__(self, root_frame, voucher_printer):
-        tk.Frame.__init__(self, root_frame)
-
-        self.voucher_printer = voucher_printer
-
-
-class VoucherPrinterGUI(tk.Tk):
+class VoucherPrinterApp(tk.Tk):
     def __init__(self, *args, **kwargs):
-
         tk.Tk.__init__(self, *args, **kwargs)
-        root_frame = tk.Frame(self)
+        self.frames = {}
+
         self.geometry("800x600")
-
-        root_frame.grid_columnconfigure(0, weight=1)
-        root_frame.grid_rowconfigure(0, weight=1)
-
         self.title("CESS Payment Voucher Printer")
 
-        self.frames = {}
-        for Frame in (ui.PrimaryFrame, ui.ResultsFrame):
-            frame = Frame(root_frame, self)
-            frame.grid(row=0, column=0, sticky="snew")
-            self.frames[Frame] = frame
+        self.init_frames()
+        self.setup_menu()
 
-        menu_bar = tk.Menu(self)
-        file_menu = tk.Menu(menu_bar, tearoff=0)
-        file_menu.add_command(label="Open Z-Tree Payoff File", command=self.open_payoff_file)
-        file_menu.add_separator()
-        file_menu.add_command(label="Exit", command=root_frame.quit)
-        menu_bar.add_cascade(label="File", menu=file_menu)
-        self.config(menu=menu_bar)
-
-        toolbar = Frame(root_frame, self)
-        open_file_icon_path = os.path.join("images", "file-text-o.png")
-        img = Image.open(open_file_icon_path)
-        tkimg = ImageTk.PhotoImage(img)
-        open_button = Button(toolbar, image=tkimg)
-
-        self.configuration = ConfigurationSettings()
         self.voucher = Voucher()
 
         self.total_rows = 1
 
-        self.show_frame(ui.PrimaryFrame)
+        self.show_frame(PrimaryFrame)
 
     def show_frame(self, frame):
         current_frame = self.frames[frame]
         current_frame.tkraise()
+
+    def init_frames(self):
+        frames = (PrimaryFrame, ResultsFrame)
+        for F in frames:
+            frame = F(self)
+            frame.grid(row=0, column=0, sticky="snew")
+            self.frames[F] = frame
+
+    def setup_menu(self):
+        menu_bar = tk.Menu(self)
+        file_menu = tk.Menu(menu_bar, tearoff=0)
+        file_menu.add_command(label="Open Z-Tree Payoff File", command=self.open_payoff_file)
+        file_menu.add_separator()
+        file_menu.add_command(label="Exit", command=self.quit)
+        menu_bar.add_cascade(label="File", menu=file_menu)
+        self.config(menu=menu_bar)
+
+    def open_payoff_file(self):
+        file_name = filedialog.askopenfilename(parent=self, filetypes=[("File", "*.pay")])
+        if self.voucher.open_file(file_name):
+            self.frames[PrimaryFrame].build_table(self.voucher.subjects, self.voucher.payoffs)
+        else:
+            messagebox.showerror("Error", "Unable to open file. Error: {}.".format(self.voucher.error))
+
+
+class PrimaryFrame(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+        self.setup_toolbar()
+        self.config_settings = ConfigurationSettings()
+
+    def setup_toolbar(self):
+        toolbar = tk.Frame(self)
+        open_file_icon_path = os.path.join("images", "folder-open-o.png")
+        open_button = ttk.Button(toolbar, command=self.parent.open_payoff_file)
+        try:
+            img = ImageTk.PhotoImage(file=open_file_icon_path)
+            open_button.config(image=img)
+            open_button.image = img
+            open_button.pack(side=tk.LEFT, padx=2, pady=2)
+        except Exception as e:
+            print("Error: {}".format(e))
+
+        toolbar.grid(row=0, column=0, sticky="nwe")
 
     def calibrate(self):
         pass
@@ -69,35 +81,28 @@ class VoucherPrinterGUI(tk.Tk):
         for row in range(1, rows):  # Rows
             a = ttk.Entry(self)
             a.insert(tk.END, subjects[row])
-            a.grid(row=self.total_rows, column=0)
+            a.grid(row=row, column=0)
 
             b = ttk.Entry(self)
             b.insert(tk.END, payoffs[row])
-            b.grid(row=self.total_rows, column=1)
+            b.grid(row=row, column=1)
 
-            self.total_rows += 1
-
+        config_label = ttk.Label(text='Select a print configuration')
+        config_label.grid(row+1, column=0)
+        combo = ttk.Combobox(self)
+        combo['values'] = self.config_settings.to_list()
         button1 = ttk.Button(self, text="Calibrate", command=lambda: self.calibrate())
-        button1.grid(row=self.total_rows, column=0)
-
-    def open_payoff_file(self):
-        file_name = filedialog.askopenfilename(parent=self, filetypes=[("File", "*.pay")])
-        if self.voucher.open_file(file_name):
-            self.build_table(self.voucher.subjects, self.voucher.payoffs)
-        else:
-            messagebox.showerror("Error", "Unable to open file. Error: {}.".format(self.voucher.error))
+        button1.grid(row=rows+1, column=0)
 
 
 class ResultsFrame(tk.Frame):
-    def __init__(self, root_frame, voucher_printer):
-        tk.Frame.__init__(self, root_frame)
-
-        self.voucher_printer = voucher_printer
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
 
         label = ttk.Label(self, text="Results", font=('Arial', 12))
         label.pack(pady=10, padx=10)
 
-        button1 = ttk.Button(self, text="Back", command=lambda: root_frame.show_frame(PrimaryFrame))
+        button1 = ttk.Button(self, text="Back", command=lambda: parent.show_frame(PrimaryFrame))
         button1.pack()
 
 
